9238d39eeb0b118f9f1d85a088809b0276ec55bb
Artyom Kayun <kayun.artem@gmail.com>
2017-04-04 18:34:18 +0600

    add ratio service & slider option

--- src/app/components/ratio/ratio.controller.js
+++ src/app/components/ratio/ratio.controller.js
@@ -1,4 +1,29 @@
 export class RatioController {
-  constructor() {
+  constructor($scope, ratioService) {
+    this.$scope = $scope;
+    this.ratioService = ratioService;
+    this.items = [];
+    this.optionsList = [1, 2, 3, 4, 5];
+    this.currentOption = null;
+  }
+
+  getOptions(option) {
+    this.ratioService.getData(option).then(
+      data => this.$scope.$apply(() => this.items = data.Items),
+      response => console.log(response.statusText)
+    );
+  }
+
+  get sliderOptions() {
+    return {
+      min: 0,
+      max: 100,
+      floor: 0,
+      ceil: 100,
+      step: 0.01,
+      precision: 2
+    }
   }
 }
+
+RatioController.$inject = ['$scope', 'ratioService']
