b60128e718b3b155e6a42ca1e4081f3d26d9ab9f
Artyom Kayun <kayun.artem@gmail.com>
2017-04-05 01:24:01 +0600

    add ratio calculation

--- src/app/components/ratio/ratio.controller.js
+++ src/app/components/ratio/ratio.controller.js
@@ -5,24 +5,91 @@ export class RatioController {
     this.items = [];
     this.optionsList = [1, 2, 3, 4, 5];
     this.currentOption = null;
+
+    this.sliderOptions = {
+      floor: 0,
+      ceil: 100,
+      step: 0.01,
+      precision: 2,
+      interval: 0,
+      onChange: this.listenerHandler.bind(this),
+      onEnd: this.listenerHandler.bind(this)
+    };
   }
 
   getOptions(option) {
     this.ratioService.getData(option).then(
-      data => this.$scope.$apply(() => this.items = data.Items),
+      data => {
+        this.items = data.Items.map(item => {
+          item.ID = this.idGenerate();
+          return item;
+        });
+
+        this.$scope.$digest(() => {
+          this.items = this.dataRatio(data.Items);
+        });
+      },
       response => console.log(response.statusText)
     );
   }
 
-  get sliderOptions() {
-    return {
-      min: 0,
-      max: 100,
-      floor: 0,
-      ceil: 100,
-      step: 0.01,
-      precision: 2
+  get MAX_SUM() {
+    return 100;
+  }
+
+  dataRatio(id = null) {
+    let sum = this.items.reduce((sum, item) => this.round(item.Percent + sum, 2), 0);
+    let diff = this.round(this.MAX_SUM - sum, 2);
+    let items = [];
+
+    if (this.items.length > 1) {
+
+      if (id) {
+        items = this.items.filter(item => item.ID !== id);
+      }
+
+      if (diff > 0) {
+        let min = this.min(items);
+        let value = this.round(min.Percent + diff, 2);
+        min.Percent = value < 0 ? 0: value;
+      } else {
+        let max = this.max(items);
+        let value = this.round(max.Percent + diff, 2);
+        max.Percent = value < 0 ? 0: value;
+      }
     }
+
+    return items;
+  }
+
+  listenerHandler(id) {
+    this.$scope.$applyAsync(() => this.dataRatio(id));
+  }
+
+  max(array) {
+    return array.reduce((prev, item) =>  item.Percent > prev.Percent ? item : prev);
+  }
+
+  min(array) {
+    return array.reduce((prev, item) =>  item.Percent < prev.Percent ? item : prev);
+  }
+
+  idGenerate(idLength = 4) {
+    const ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+    let rtn = '';
+    for (let i = 0; i < idLength; i++) {
+      rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+    }
+    return rtn;
+  }
+
+  extendOptions(obj) {
+    return angular.extend(this.sliderOptions, obj);
+  }
+
+  round(num, precision) {
+    return Math.ceil(num * 10 ** precision) / 10 ** precision;
   }
 }
 
